a22bd4b9727cac7a5f67e95ceb678ba2501f71a5
ct-model-school <shafa.ctmodel@gmail.com>
2026-08-23 12:36:43 +0600

    Load mobile community refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./store-inventory-mobile.css";
 import "./mobile-responsive.css";
 import "./hero-fix.css";
 import "./contact-enhancements.css";
+import "./community-mobile.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import PublicPeopleNav from "@/app/components/PublicPeopleNav";
 
