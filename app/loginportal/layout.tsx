677eea81cd588fd346cd44e2b35fb69133caff2b
ct-model-school <shafa.ctmodel@gmail.com>
2026-08-24 18:52:06 +0600

    Isolate member login token per browser tab

---
+++ app/loginportal/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, type ReactNode } from "react";
+
+const TOKEN_KEY = "ctms_store_token";
+
+export default function LoginPortalLayout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const originalSetItem = Storage.prototype.setItem;
+    const originalRemoveItem = Storage.prototype.removeItem;
+
+    Storage.prototype.setItem = function (key: string, value: string) {
+      if (this === window.localStorage && key === TOKEN_KEY) {
+        window.sessionStorage.setItem(TOKEN_KEY, value);
+        return;
+      }
+      return originalSetItem.call(this, key, value);
+    };
+
+    Storage.prototype.removeItem = function (key: string) {
+      if (this === window.localStorage && key === TOKEN_KEY) {
+        window.sessionStorage.removeItem(TOKEN_KEY);
+        return;
+      }
+      return originalRemoveItem.call(this, key);
+    };
+
+    return () => {
+      Storage.prototype.setItem = originalSetItem;
+      Storage.prototype.removeItem = originalRemoveItem;
+    };
+  }, []);
+
+  return children;
+}
