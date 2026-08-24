1122ed37c733bef81ce44ada4ac3c2a3786f5fea
ct-model-school <shafa.ctmodel@gmail.com>
2026-08-24 18:52:19 +0600

    Read member session token from per-tab storage

---
+++ app/member/dashboard/layout.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, type ReactNode } from "react";
+
+const TOKEN_KEY = "ctms_store_token";
+
+export default function MemberDashboardLayout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const originalGetItem = Storage.prototype.getItem;
+    const originalRemoveItem = Storage.prototype.removeItem;
+
+    Storage.prototype.getItem = function (key: string) {
+      if (this === window.localStorage && key === TOKEN_KEY) {
+        return window.sessionStorage.getItem(TOKEN_KEY);
+      }
+      return originalGetItem.call(this, key);
+    };
+
+    Storage.prototype.removeItem = function (key: string) {
+      if (this === window.localStorage && key === TOKEN_KEY) {
+        window.sessionStorage.removeItem(TOKEN_KEY);
+        return;
+      }
+      return originalRemoveItem.call(this, key);
+    };
+
+    return () => {
+      Storage.prototype.getItem = originalGetItem;
+      Storage.prototype.removeItem = originalRemoveItem;
+    };
+  }, []);
+
+  return children;
+}
